1bb34bdb97283bbbfd10ac3ad5235c2b8158c103
unknown <BFROKLAG@LBNL04132.corp.capgemini.com>
2014-01-24 08:55:48 +0100

    removed android

--- README.md
+++
@@ -1,4 +0,0 @@
-pocketmoney
-===========
-
-An educational experience to learn children manage their pocket money
--- gen/com/capgemini/pocketmoney/BuildConfig.java
+++
@@ -1,6 +0,0 @@
-/** Automatically generated file. DO NOT MODIFY */
-package com.capgemini.pocketmoney;
-
-public final class BuildConfig {
-    public final static boolean DEBUG = true;
-}
