94ffd0b8e39ef57fb68a4fe15763b92ef46c044c
geertjanb <geertjan88@gmail.com>
2014-01-24 16:05:34 +0100

    login page style

--- scripting/login_main.js
+++ scripting/login_main.js
@@ -2,12 +2,13 @@ define(["jquery"], function($) {
 
 	var init = function(callback) {
 		var $widget = $('#div_login');
+		var $placeHolder = $('#login_placeholder');
 		
 		addButtons = function(amount) {
-			for(var i = 0; i < amount; i++) {
-				$widget.append('<div class="button" id="button-'+i+'"></div>');
+			for(var i = 1; i <= amount; i++) {
+				$placeHolder.append('<div class="button" id="button-'+i+'"></div>');
 				var $button = $('#button-'+i);
-				alert($button);
+				$button.css("background-image","url('media/images/login/owl-"+i+".png')");
 			}
 		};
 		
