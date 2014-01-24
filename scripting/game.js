24e0ed7d2fa671c5566cd10b2dddc47b9f4dc743
ov0364 <overbeek@gmail.com>
2014-01-24 17:56:24 +0100

    flying

--- scripting/game.js
+++ scripting/game.js
@@ -2,15 +2,60 @@ define(["jquery", "jquery-scrolly", "jquery-ui-touch-punch"], function ($) {
     //Do setup work her
     var init = function() {
       $("#div_main").show();
+	  
       $('.parallax').scrolly({bgParallax: true});
-      $( "#draggable" ).draggable().droppable({
-			drop: function( event, ui ) {
-				$('#draggable').css({'border': '5px solid green'});
+	  
+	  var level = 1, isFlying = false;
+	  
+		$( "#draggable" ).draggable({
+			drag: function( event, ui ) {
+				
+				
+				var top = $('#draggable').css('top').replace('px', '');
+				
+				//console.log($('#draggable').css('top'), top)
+				if (level === 1 && top < 100 ) {
+					console.log('fall');
+					$('body').focus();
+					$('#draggable').animate({top: '400px'}, 1000);
+				} 
+
+				
+				//osition: relative; left: 376px; top: 167px;
 			},
-			out: function (event, ui) {
-				$('#draggable').css({'border': ''});
+			start : function() {
+				console.log('draggable start');
+				isFlying = true;
+				function fly () {
+					if ($('#draggable img').attr('src') === 'media/images/game/Character_Owl_2_fly1.png') {
+						$('#draggable img').attr('src', 'media/images/game/Character_Owl_2_fly2.png');
+					} else {
+						$('#draggable img').attr('src', 'media/images/game/Character_Owl_2_fly1.png');
+					}
+
+					if (isFlying) {
+						setTimeout(function(){
+							fly();
+						}, 5);
+					}
+				}
+				fly();
+			},
+			stop : function() {
+				console.log('draggable stop');
+				isFlying = false;
+				$('#draggable img').attr('src', 'media/images/game/Character_Owl_2.png');
 			}
 		});
+		
+		$( "#droppable" ).droppable({
+				drop: function( event, ui ) {
+					$('#draggable').css({'border': '5px solid green'});
+				},
+				out: function (event, ui) {
+					$('#draggable').css({'border': ''});
+				}
+			});
     };
 
     return {
