2262e5f5c262499c5ce609c47e9edab1c7406d13
bartfroklage <bart.froklage@gmail.com>
2014-01-24 23:33:12 +0100

    Merge branch 'master' of https://github.com/jekkos/pocketmoney
    Conflicts:
    	app/config.js

--- app/config.js
+++ app/config.js
@@ -10,7 +10,7 @@ require.config({
     "jquery-scrolly" : "../scripting/jquery.scrolly",
     "login" : "../scripting/login_main",
 	"userservice" : "../scripting/user_service",
-	"statusbar" : "../scripting/statusbar"
+	"statusbar" : "../scripting/statusbar",
 	"createjs" : "../scripting/createjs-2013.12.12.min"
   }, 
   shim: {
