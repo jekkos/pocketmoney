1fdeb37d4cae5d4794a923c1aab6e27f847eac6a
unknown <BFROKLAG@LBNL04132.corp.capgemini.com>
2014-01-24 16:37:14 +0100

    add loginservice initial

--- scripting/login_service.js
+++ scripting/login_service.js
@@ -1,7 +1,12 @@
 define(["jquery"], function($) {
 
-
-
-
-
+	function checkLogin(code) {
+		$.ajax({
+			dataType: "json",
+			url: "mock/users.json",
+		}).done(function() {
+			alert('done');
+		});
+	
+	}
 });
