b328cf0994e358d90b3f5f105b9284e8edba59a9
unknown <BFROKLAG@LBNL04132.corp.capgemini.com>
2014-01-24 19:44:54 +0100

    updated

--- app/config.js
+++ app/config.js
@@ -1,18 +1,19 @@
 require.config({
   // make components more sensible
   // expose jquery 
-  paths: {
+  paths: {	
     "components": "../bower_components",
     "jquery": "../bower_components/jquery/jquery",
     "background" : "../scripting/background",
     "scrolly" : "../scripting/jquery.scrolly",
     "login" : "../scripting/login_main",
-	"loginservice" : "../scripting/login_sevice"
+	"userservice" : "../scripting/user_service"
   }
 });
 
 if (!window.requireTestMode) {
   require(['main'], function() {
+	
 	  
   });
 }
