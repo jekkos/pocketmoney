8699e9ed041d3388b5e2c00ee761113f0b7837ba
jekkos-t520 <jeroen.peelaerts@gmail.com>
2014-01-25 11:03:08 +0100

    Cleanup game.js, create owl.js for owl animation

--- scripting/game.js
+++ scripting/game.js
@@ -1,8 +1,7 @@
-define(["jquery", "createjs", "jquery-scrolly", "jquery-ui-touch-punch"], function ($, createjs) {
+define(["jquery", "createjs", "owl", "jquery-scrolly", "jquery-ui-touch-punch"], function ($, createjs, owl) {
     //Do setup work her
     var init = function() {
     	
-    	console.log(createjs);
       $("#div_main").show();
 	  
       //find canvas and load images, wait for last image to load
@@ -11,213 +10,29 @@ define(["jquery", "createjs", "jquery-scrolly", "jquery-ui-touch-punch"], functi
       // create a new stage and point it at our canvas:
       var stage = new createjs.Stage(canvas);      
 
-//      // enable touch interactions if supported on the current device:
-//      createjs.Touch.enable(stage);
-//
-//      // enabled mouse over / out events
-//      stage.enableMouseOver(10);
-//      stage.mouseMoveOutside = true; // keep tracking the mouse even when it leaves the canvas      
-      
       //wait for the image to load
       var imgTree = new Image();
-      imgTree.onload = handleImageLoad;
+      imgTree.onload = function(event) {
+    	  var tree = new createjs.Bitmap(event.target);
+          stage.addChild(tree);               
+          
+          var newOwl = owl.create();
+          newOwl.run();
+          stage.addChild(newOwl);
+          
+    	  stage.addEventListener("stagemousedown", newOwl.fly);
+    	  
+          createjs.Ticker.timingMode = createjs.Ticker.RAF;
+          createjs.Ticker.addEventListener("tick", function(event) {
+        	  console.log('tick');
+        	  newOwl.update();
+              // this set makes it so the stage only re-renders when an event handler indicates a change has happened.
+              stage.update(event);
+          });
+      };
       imgTree.src = "media/images/game/BG_tree.png";    
 
-      //wait for the image to load
-      var imgOwl, imgEx;    
-
-      var update = false;
-      
-      function handleImageLoad (event) {
-    	  var bg = new createjs.Bitmap(event.target);
-          stage.addChild(bg);               
-          stage.update();
-          
-          imgOwl = new Image();
-          imgOwl.onload = handleOwlLoad;
-          imgOwl.src = "media/images/game/Character_Owl_2.png";    
-          
-          imgEx = new Image();
-          imgEx.onload = handleExample;
-          imgEx.src = "media/images/game/Character_Owl_2_sprite.png"; 
-      }
-      
-      var grant;
-      
-      function handleExample() {
-    	  
-          var data = new createjs.SpriteSheet({
-              "images": [imgEx],
-              "frames": {"regX": 0, "height": 300, "count": 9, "regY": 0, "width": 300},
-              // define two animations, run (loops, 1.5x speed) and jump (returns to run):
-              "animations": {"sleep": [0, 2], "fly": [3, 4, "fly", 500]}
-	      });
-	      grant = new createjs.Sprite(data, "sleep");
-	      grant.setTransform(100, 90, 0.8, 0.8);
-	      grant.framerate = 30;
-	      stage.addChild(grant);
-	      
-	      stage.addEventListener("stagemousedown", function () {
-              grant.gotoAndPlay("fly");
-	        });	      
-	      
-	    	      
-	      
-        // using "on" binds the listener to the scope of the currentTarget by default
-        // in this case that means it executes in the scope of the button.
-        grant.on("mousedown", function(evt) {
-      	  		console.log('mousedown');
-                this.parent.addChild(this);
-                this.offset = {x:this.x-evt.stageX, y:this.y-evt.stageY};
-        });
-        
-        // the pressmove event is dispatched when the mouse moves after a mousedown on the target until the mouse is released.
-        grant.on("pressmove", function(evt) {
-                this.x = evt.stageX+ this.offset.x;
-                this.y = evt.stageY+ this.offset.y;
-                // indicate that the stage should be updated on the next tick:
-                update = true;
-        });
-        
-        grant.on("rollover", function(evt) {
-                this.scaleX = this.scaleY = this.scale*1.2;
-                update = true;
-        });
-        
-        grant.on("rollout", function(evt) {
-                this.scaleX = this.scaleY = this.scale;
-                update = true;
-        });  	      
-	      stage.update();
-      }
-      
-      function handleOwlLoad() {
-    	  
-          var data = new createjs.SpriteSheet({
-	              "images": [imgOwl],
-	              "frames": {"regX": 0, "height": 100, "count": 3, "regY": 0, "width": 100},
-	              // define two animations, run (loops, 1.5x speed) and jump (returns to run):
-	              "animations": {"run": [0, 2, "run", 1.5]}
-	      });
-	      var grant = new createjs.Sprite(data, "run");
-          //grant.setTransform(-200, 90, 0.8, 0.8);
-          grant.framerate = 30;  	  
-    	  
-	      //stage.addChild(grant);
-	      stage.update();
-//	      
-	      
-	      //stage.addEventListener("stagemousedown", handleJumpStart);
-	      
-//    	  var bitmap = new createjs.Bitmap(imgOwl);
-//          stage.addChild(bitmap);               
-//          bitmap.cursor = "pointer";
-//          
-//          // using "on" binds the listener to the scope of the currentTarget by default
-//          // in this case that means it executes in the scope of the button.
-//          bitmap.on("mousedown", function(evt) {
-//        	  		console.log('mousedown');
-//                  this.parent.addChild(this);
-//                  this.offset = {x:this.x-evt.stageX, y:this.y-evt.stageY};
-//          });
-//          
-//          // the pressmove event is dispatched when the mouse moves after a mousedown on the target until the mouse is released.
-//          bitmap.on("pressmove", function(evt) {
-//                  this.x = evt.stageX+ this.offset.x;
-//                  this.y = evt.stageY+ this.offset.y;
-//                  // indicate that the stage should be updated on the next tick:
-//                  update = true;
-//          });
-//          
-//          bitmap.on("rollover", function(evt) {
-//                  this.scaleX = this.scaleY = this.scale*1.2;
-//                  update = true;
-//          });
-//          
-//          bitmap.on("rollout", function(evt) {
-//                  this.scaleX = this.scaleY = this.scale;
-//                  update = true;
-//          });          
-          
-	      createjs.Ticker.timingMode = createjs.Ticker.RAF;
-          createjs.Ticker.addEventListener("tick", tick);
-          stage.update();
-      }
-      
-      function tick(event) {
-    	  console.log('tick');
-    	  
-          grant.x = grant.x + 0.1;
-          grant.y = grant.y + 0.5;    	  
-    	  
-          // this set makes it so the stage only re-renders when an event handler indicates a change has happened.
-          if (update) {
-        	  		console.log('tick');
-                  update = false; // only update once
-                  stage.update(event);
-          }
-          stage.update(event);
-      }      
-      
       $('.parallax').scrolly({bgParallax: true});
-	  
-//	  var level = 1, isFlying = false;
-//	  
-//		$( "#draggable" ).draggable({
-//			drag: function( event, ui ) {
-//				var top = $('#draggable').css('top').replace('px', '');
-//				
-//				if (level === 1 && top < 100 ) {
-//					console.log('fall');
-//					$('body').focus();
-//					$('#draggable').animate({top: '500px'}, 100);
-//				} 
-//
-//			},
-//			start : function() {
-//				console.log('draggable start');
-//				isFlying = true;
-//				function fly () {
-//					if ($('#draggable img').attr('src') === 'media/images/game/Character_Owl_2_fly1.png') {
-//						$('#draggable img').attr('src', 'media/images/game/Character_Owl_2_fly2.png');
-//					} else {
-//						$('#draggable img').attr('src', 'media/images/game/Character_Owl_2_fly1.png');
-//					}
-//
-//					if (isFlying) {
-//						setTimeout(function(){
-//							fly();
-//						}, 30);
-//					} else {
-//						console.log("stop flying");
-//					}
-//				}
-//				fly();
-//			},
-//			stop : function() {
-//				console.log('draggable stop');
-//				isFlying = false;
-//				$('#draggable img').attr('src', 'media/images/game/Character_Owl_2.png');
-//			}
-//		});
-//		
-//		$( "#droppable" ).droppable({
-//				drop: function( event, ui ) {
-//					//$('#draggable').css({'border': '5px solid green'});
-//					
-//					// Sleep
-//					setTimeout(function(){
-//						$('#draggable img').attr('src', 'media/images/game/Character_Owl_2_eye1.png');
-//						setTimeout(function(){
-//							$('#draggable img').attr('src', 'media/images/game/Character_Owl_2_eye2.png');
-//						}, 100);
-//					}, 30);
-//					
-//				},
-//				out: function (event, ui) {
-//					$('#draggable').css({'border': ''});
-//				}
-//			});
     };
 
     return {
