3c62f2e86642a73f2e4baf7a952879309cc7ec17
bartfroklage <bart.froklage@gmail.com>
2014-01-25 09:54:07 +0100

    updated

--- app/main.js
+++ app/main.js
@@ -1,12 +1,12 @@
 // to depend on a bower installed component:
 // define(['bower_components/componentName/file'])
 
-define(["game", "login", "statusbar"], function(game, login) {
+define(["game", "login", "statusbar"], function(game, login, statusbar) {
 	
 	login.init(function(event) {
 		  login.hide();
 		  game.init();
-		  //statusbar.init();
+		  statusbar.init();
     });
 	
 });
--- scripting/statusbar.js
+++ scripting/statusbar.js
@@ -2,8 +2,28 @@ define(["jquery"], function ($) {
 
     //Do setup work her
     var init = function() {
+		var noAcorns = 5;
+		var noEnergy = 2;
+							
+		setAcorns = function(acorns) {
+			$('div#acorns tr').empty();
+			for (var i = 0; i < noAcorns; i++) {
+				$('div#acorns tr').append('<td><img src="media/images/acorn_'+ ((i > acorns) ? 'on' : 'off') +'.png"/></td>');
+			}		
+		};
 		
+		setEnergy = function(engergy) {
+			for (var i = 0; i < noEnergy; i++) {				
+				
+				$('div#energy tr').append('<td><img src="media/images/energy_'+ ((true) ? '1' : '2') +'.png"/></td>');
+			}		
+		};
+		
+		setAcorns(0);
+		setEnergy(0);
     };
+	
+	
 
     return {
       init : init
